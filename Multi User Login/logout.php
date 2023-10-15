3bef58d25d84ce778f69b186a93a22af5d687fe6
Aditya N Sharma <adityansharma05@gmail.com>
2023-10-15 20:53:17 +0530

    Add files via upload

--- Multi User Login/logout.php
+++ Multi User Login/logout.php
@@ -3,6 +3,12 @@
 <html>
 <head>
     <link rel="stylesheet" href="mystyle.css" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+      html,body{
+        overflow: hidden;
+      }
+    </style>
 </head>
 <body>
     
@@ -36,10 +42,24 @@
     </div>
   </div>
 </div>
+<div class="container d-flex justify-content-center 
+    align-items-center"
+    style="margin-top:200px;width:400px;">
 <form action="" method="POST"> 
-    <label for="out">Do you want to logout?</label>
-  <input type="submit" name="Logout" value="Logout" id="out"> 
+  
+<div class="card" style="width: 600px;">
+
+  <div class="card-body">
+    <h5 class="card-title" style="text-align:center">Logout</h5>
+    <p class="card-text">Do you really want to logout? </p>
+    <input class="btn btn-primary" type="submit" name="Logout" value="Logout" id="out"> 
+    
+  </div>
+</div>
+    <label for="out"></label>
+  
 </form> 
+</div>
 <?php 
 
 if(isset($_POST['Logout']))
