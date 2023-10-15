06cb7d49f399e611a1b603add8fb1745193e33d4
Aditya N Sharma <adityansharma05@gmail.com>
2023-10-15 10:56:17 +0530

    Add files via upload

--- Multi User Login/home.php
+++ Multi User Login/home.php
@@ -1,7 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="mystyle.css" type="text/css">
+     <!-- <style>
+            .aboutus
+      {
+        margin-top:100px;
+        text-align: center;
+      }
+      .aboutcon
+      {
+        text-align: justify;
+      justify-content: center;
+      margin-left: 30%;
+        margin-right: 30%;
+      }
+      body{
+        overflow:hidden;
+      }
+    </style>  -->
 </head>
 <body>
     
@@ -33,6 +50,19 @@
           <i class="fa fa-bars"></i>
         </label>
     </div>
+  </div>
+</div>
+
+<div>
+  <div class="aboutus">About Us</div>
+  <div class="aboutcon" style="color:white;">
+  <section>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique hic quasi voluptatum sapiente nostrum porro, laboriosam ullam aliquid quae nemo.  
+  </section>  
+  <section>
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus qui illo eum quidem nostrum assumenda, distinctio, asperiores nam, fugiat tenetur error quasi eveniet odio itaque esse vitae non beatae fugit odit soluta necessitatibus officiis quae? Delectus iste beatae corporis incidunt obcaecati veniam aspernatur? Obcaecati officia sequi iure, culpa maiores totam!
+  </section>
   </div>
 </div>
 </body>
